cac47cddda22820534f7b176d769a5010adb74e7
wzy810103882 <zw2585@columbia.edu>
2019-11-11 16:07:10 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -24,7 +24,7 @@ input_width  = 640
 
 def test_simple(model):
     #imgfolder = ''
-    savefolder = '/work/NYUv2_DE'
+    
 
     test_img_names = sorted(glob.glob("/work/NYUv2/nyu_test_rgb/*.png"))
     train_img_names = sorted(glob.glob("/work/NYUv2/nyu_train_rgb/*.png"))
@@ -35,8 +35,11 @@ def test_simple(model):
     model.switch_to_eval()
     print(len(test_img_names))
     print(len(train_img_names))
+    test_depth = np.zeros((len(test_img_names),480,640))
+    train_depth = np.zeros((len(train_img_names),480,640))
+
     for i in range(len(test_img_names)):
-        print("first_iteration")
+        #print("first_iteration")
         img_path = test_img_names[i]
         img = np.float32(io.imread(img_path))/255.0
         img = resize(img, (input_height, input_width), order = 1)
@@ -51,8 +54,32 @@ def test_simple(model):
 
 
         pred_depth = pred_depth.data.cpu().numpy()
-        print(pred_depth.shape)
+        test_depth[i,:,:] = pred_depth
 
+        #print(pred_depth.shape)
+        
+    for i in range(len(train_img_names)):
+    #print("first_iteration")
+        img_path = train_img_names[i]
+        img = np.float32(io.imread(img_path))/255.0
+        img = resize(img, (input_height, input_width), order = 1)
+        input_img =  torch.from_numpy( np.transpose(img, (2,0,1)) ).contiguous().float()
+        input_img = input_img.unsqueeze(0)
+
+        input_images = Variable(input_img.cuda())
+        pred_log_depth = model.netG.forward(input_images) 
+        pred_log_depth = torch.squeeze(pred_log_depth)
+        
+        pred_depth = torch.exp(pred_log_depth)
+
+
+        pred_depth = pred_depth.data.cpu().numpy()
+        train_depth[i,:,:] = pred_depth
+    #print(pred_depth.shape)
+
+    savefolder = '/work/NYUv2_DE'
+    np.savez("%s/%s_depth_estimation_megadepth.npz" % (savefolder, "train"), train_depth)
+    np.savez("%s/%s_depth_estimation_megadepth.npz" % (savefolder, "test"), test_depth)
 
     # visualize prediction using inverse depth, so that we don't need sky segmentation (if you want to use RGB map for visualization, \
     # you have to run semantic segmentation to mask the sky first since the depth of sky is random from CNN)
